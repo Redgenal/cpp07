08db633f283ec30d7187fe4c0ec25dc1707faf10
Sergey <50015502+Redgenal@users.noreply.github.com>
2022-11-09 23:44:13 +0300

    123

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: utawana <utawana@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 21:00:33 by utawana           #+#    #+#             */
+/*   Updated: 2022/11/09 21:00:33 by utawana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ITER_H
 #define  ITER_H
 
 #include <iostream>
 
 template <typename T>
-void iter(T &arr, int len, void (*f)(T &elem)) {
+void iter(T* arr, int len, void(*f)(T const &elem)) {
     for (int i = 0; i < len; i++)
         f(arr[i]);
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: utawana <utawana@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 21:00:30 by utawana           #+#    #+#             */
+/*   Updated: 2022/11/09 21:00:30 by utawana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "iter.hpp"
 
-template <typename T = int>
+template <typename T>
 void    test(T const &elem) {
     std::cout << "This is " << elem << std::endl;
 }
@@ -9,7 +21,7 @@ int main() {
     int arr1[5] = {1, 2, 3, 4, 5};
     std::string arr2[5] = {"hello", "hi", "hola", "bonjour", "ola"};
 
-    ::iter(arr1, 5, test<int>);
-    ::iter(arr2, 5, test);
+    iter(arr1, 5, test<int>);
+    iter(arr2, 5, test);
     return (0);
 }
